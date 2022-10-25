c13c66da33ac9f772617da357538e07992d73cd4
Marcos Rodrigues <mquinhoz@hotmail.com>
2022-10-25 13:48:37 -0300

    feat: add access button function

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,1 +1,30 @@
-/* Desenvolva seu código aqui */
+import {login} from "../../scripts/api.js"
+
+const inputs = document.querySelectorAll("input");
+const emailInput = document.getElementById("email-login");
+const passwordInput = document.getElementById("password-login");
+const accessBtn = document.getElementById("access");
+ 
+accessBtn.addEventListener("click", async (e) =>{
+e.preventDefault()
+  const submit = {
+    email: `${emailInput.value}`,
+    password: `${passwordInput.value}`
+  }
+  console.log(submit)
+  const token = await login(submit);
+  localStorage.setItem("token",`${token.token}`)
+  /* window.location.assign("/pages/register.html") */
+})
+
+
+passwordInput.addEventListener("keyup", (input) => {
+if(input.target.value !== "" && emailInput.value !== ""){
+  accessBtn.classList.add("on-btn");
+      accessBtn.disabled = false;
+} else {
+  accessBtn.classList.remove("on-btn");
+  accessBtn.disabled = true;
+}
+}
+)
--- scripts/api.js
+++ scripts/api.js
@@ -1,18 +1,19 @@
 export async function login(user) {
-  const data = await fetch(`https:localhost:3333/login`, {
+  
+  const data = await fetch(`http://localhost:3333/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(user),
   })
     .then((res) => res.json())
-    .then((res) => res)
+    .then((res) =>  res)
     .catch((error) => console.log(error));
 
   return data;
 }
 
 export async function register(user) {
-  const data = await fetch(`https:localhost:3333/users/create`, {
+  const data = await fetch(`http://localhost:3333/users/create`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(user),
@@ -25,11 +26,11 @@ export async function register(user) {
 }
 
 export async function search(token) {
-  const data = await fetch(`https:localhost:3333/users/profile`, {
+  const data = await fetch(`http://localhost:3333/users/profile`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
+      "Authorization": `Bearer ${token}`,
     },
   })
     .then((res) => res.json())
@@ -40,7 +41,7 @@ export async function search(token) {
 }
 
 export async function update(user, token) {
-  const data = await fetch(`https:localhost:3333/users/profile`, {
+  const data = await fetch(`http://localhost:3333/users/profile`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
@@ -56,7 +57,7 @@ export async function update(user, token) {
 }
 
 export async function remove(token) {
-  const data = await fetch(`https:localhost:3333/users/profile`, {
+  const data = await fetch(`http://localhost:3333/users/profile`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
@@ -71,7 +72,7 @@ export async function remove(token) {
 }
 
 export async function post(body, token) {
-  const data = await fetch(`https:localhost:3333/posts/create`, {
+  const data = await fetch(`http://localhost:3333/posts/create`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -87,7 +88,7 @@ export async function post(body, token) {
 }
 
 export async function searchPost(token) {
-  const data = await fetch(`https:localhost:3333/posts/create`, {
+  const data = await fetch(`http://localhost:3333/posts/create`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -102,7 +103,7 @@ export async function searchPost(token) {
 }
 
 export async function updatePost(id, token, body) {
-  const data = await fetch(`https:localhost:3333/posts/${id}`, {
+  const data = await fetch(`http:localhost:3333/posts/${id}`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
@@ -118,7 +119,7 @@ export async function updatePost(id, token, body) {
 }
 
 export async function removePost(id, token) {
-  const data = await fetch(`https:localhost:3333/posts/${id}`, {
+  const data = await fetch(`http://localhost:3333/posts/${id}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
