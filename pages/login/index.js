1440dedbb7f2623ce81cc4d06b3aa59efcb6593c
Marcos Rodrigues <mquinhoz@hotmail.com>
2022-10-25 14:59:46 -0300

    feat: add error alert at input

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,25 +1,35 @@
 import {login} from "../../scripts/api.js"
 
-const inputs = document.querySelectorAll("input");
 const emailInput = document.getElementById("email-login");
 const passwordInput = document.getElementById("password-login");
 const accessBtn = document.getElementById("access");
- 
+let token = localStorage.getItem("token")
+localStorage.removeItem("token")
+
 accessBtn.addEventListener("click", async (e) =>{
 e.preventDefault()
   const submit = {
     email: `${emailInput.value}`,
     password: `${passwordInput.value}`
   }
-  console.log(submit)
-  const token = await login(submit);
-  localStorage.setItem("token",`${token.token}`)
-  /* window.location.assign("/pages/register.html") */
+  
+  const req = await login(submit);
+  
+  console.log(req.message)
+  if(req.message){
+    passwordInput.insertAdjacentHTML('afterend', `<p class="alert">${req.message}<p>`)
+    passwordInput.classList.remove("input")
+    passwordInput.classList.add("input-alert")
+} else {
+  localStorage.setItem("token",`${JSON.parse(req.token)}`)
+  window.location.assign("/pages/home.html")}
 })
 
 
 passwordInput.addEventListener("keyup", (input) => {
 if(input.target.value !== "" && emailInput.value !== ""){
+  passwordInput.classList.remove("input-alert")
+  passwordInput.classList.add("input")
   accessBtn.classList.add("on-btn");
       accessBtn.disabled = false;
 } else {
